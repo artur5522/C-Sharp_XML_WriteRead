aeb533aee0255c058c60d41274d713cefc251a3c
ARTURO FERNANDO <73620010+artur5522@users.noreply.github.com>
2021-09-29 15:29:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace labo3_tp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            Write.CreateXmlDocumento();
+
+            Read.ReadXML();
+
+        }
+    }
+}
